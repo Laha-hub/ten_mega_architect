466a7ed2dc88f452df0d0956f6b5e6701da39876
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-12 16:27:01 +0900

    #6 make project Responsive and create Loading Spinner

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 'use strict';
 
 {
+    // loading
+    // window.onload = () => {
+    window.addEventListener('load', () => {
+        setTimeout(() => {
+            document.querySelector('body').classList.add('display');
+        }, 2000);
+    });
+
     // hamburger-menu
     document.querySelector('.hamburger-menu').addEventListener('click', () => {
         document.querySelector('.container').classList.toggle('change');
@@ -8,10 +16,10 @@
 
 
     // scroll-top
-    document.querySelector('.scroll-btn').addEventListener('click', () => {
-        document.querySelector('html').style.scrollBehavior = 'smooth';
-        setTimeout(() => {
-            document.querySelector('html').style.scrollBehavior = 'unset';
-        }, 1000);
-    });
+    // document.querySelector('.scroll-btn').addEventListener('click', () => {
+    //     document.querySelector('html').style.scrollBehavior = 'smooth';
+    //     setTimeout(() => {
+    //         document.querySelector('html').style.scrollBehavior = 'unset';
+    //     }, 1000);
+    // });
 }
