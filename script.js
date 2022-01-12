99504f6b5f1ca5481871bebd9b0d96ccea5c302c
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2022-01-12 15:09:25 +0900

    #5 create and style Contact Section

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 'use strict';
 
 {
+    // hamburger-menu
     document.querySelector('.hamburger-menu').addEventListener('click', () => {
         document.querySelector('.container').classList.toggle('change');
+    });
+
+
+    // scroll-top
+    document.querySelector('.scroll-btn').addEventListener('click', () => {
+        document.querySelector('html').style.scrollBehavior = 'smooth';
+        setTimeout(() => {
+            document.querySelector('html').style.scrollBehavior = 'unset';
+        }, 1000);
     });
 }
